6450244d5f028d5009b1273b1c2b795d1412b295
Davi de Jesus <davi.santosoliveira214@gmail.com>
2026-09-07 16:39:24 -0300

    refactor(banner): migra estilos para tailwindcss e remove css module

--- front-end/src/components/Banner.jsx
+++ front-end/src/components/Banner.jsx
@@ -1,11 +1,10 @@
-import styles from '../css/Banner.module.css'
 import bannerImg from '../assets/imgs/banner.png'
 
 export default function Banner() {
     return (
-        <div className={styles.banner}>
-            <img src={bannerImg} alt="" />
-            <h1>Bem Vindo</h1>
+        <div className={`flex justify-center items-center relative w-full h-[70%]`}>
+            <img src={bannerImg} alt="" className='w-full h-full object-cover' />
+            <h1 className='absolute text-7xl italic font-bold text-branco text-center text-shadow-[3px_1px_3px] text-shadow-laranja-500'>Bem Vindo</h1>
         </div>
     )
 }
