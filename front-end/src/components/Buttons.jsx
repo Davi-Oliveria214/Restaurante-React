2639b3035256acb75b31e9ec748341579f53e81f
Davi de Jesus <davi.santosoliveira214@gmail.com>
2026-09-07 16:54:44 -0300

    refactor(buttons): migra estilos para o tailwindcss e remove module css

--- front-end/src/components/Buttons.jsx
+++ front-end/src/components/Buttons.jsx
@@ -1,13 +1,13 @@
-import styles from '../css/Buttons.module.css'
+const button = 'font-bold text-[1rem] p-1 text-preto border-2 border-preto rounded-sm cursor-pointer duration-300 transition-all ease-in hover:bg-[var(--cor-btn)] hover:-translate-y-1'
 
 export function Agendar() {
     return (
-        <button type='button' className={`${styles.button} ${styles.btnAgendar}`}>Agendar</button>
+        <button type='button' className={`${button} bg-green-800 [--cor-btn:bg-green-600]`}>Agendar</button>
     )
 }
 
 export function Detalhes() {
     return (
-        <button type='button' className={`${styles.button} ${styles.btnDetalhes}`}>Detalhes</button>
+        <button type='button' className={`${button} bg-laranja-500 [--cor-btn:bg-laranja-400]`}>Detalhes</button>
     )
 }
